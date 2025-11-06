ff8e9f841123ede1e64c3b2b019ab8f89d09f21e
Lauri <tikkalauri1@gmail.com>
2025-11-06 12:27:09 +0200

    Tehtävä 5

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -33,12 +33,27 @@ def main():
         if not players:
             console.print(f"[error]No players of nationality [magenta]{input}[/magenta] found![/error]")
 
-
     # Create table
+        table = Table(title=f"NHL players from ({input})", style="bold white")
+        table.add_column("Name", justify="left", style="player", no_wrap=True)
+        table.add_column("Team", justify="center", style="team")
+        table.add_column("Goals", justify="center", style="stats")
+        table.add_column("Assists", justify="center", style="stats")
+        table.add_column("Points", justify="center", style="bold yellow")
 
     # Add data to table
+        for player in players:
+            table.add_row(
+                player.name,
+                player.team,
+                str(player.goals),
+                str(player.assists),
+                str(player.get_goals_assists())
+            )
 
     # Print table
+        console.print()
+        console.print(table)
 
 if __name__ == "__main__":
     main()
