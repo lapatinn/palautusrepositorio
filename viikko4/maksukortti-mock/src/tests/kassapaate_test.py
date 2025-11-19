94fe0fd6bda30d48a222e2643780dd88b73f1558
Lauri <tikkalauri1@gmail.com>
2025-11-19 17:12:19 +0200

    Viikko4, tehtävät 1-2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,18 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_maksupaate_lataa_rahan_kortille(self):
+        maksukortti_mock = Mock()
+
+        maksukortti_mock.saldo.return_value = 0
+
+        self.kassa.lataa(maksukortti_mock, HINTA)
+
+        maksukortti_mock.lataa.assert_called_with(HINTA)
+
+    def test_maksupaate_ei_lataa_negatiivista_summaa(self):
+        maksukortti_mock = Mock()
+        self.kassa.lataa(maksukortti_mock, -HINTA)
+
+        maksukortti_mock.lataa.assert_not_called()
