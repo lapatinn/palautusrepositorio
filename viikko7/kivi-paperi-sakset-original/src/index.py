2cba306a82426f6175ce9a9f7c1f2048a232200d
Lauri <tikkalauri1@gmail.com>
2025-12-11 17:30:34 +0200

    Kopioi kps-peli (viikko7 tehtävä 5)

---
+++ viikko7/kivi-paperi-sakset-original/src/index.py
@@ -0,0 +1,25 @@
+from kps import KiviPaperiSakset
+
+def main():
+    while True:
+        print("Valitse pelataanko"
+              "\n (a) Ihmistä vastaan"
+              "\n (b) Tekoälyä vastaan"
+              "\n (c) Parannettua tekoälyä vastaan"
+              "\nMuilla valinnoilla lopetetaan"
+              )
+
+        vastaus = input()
+        if vastaus in ["a", "b", "c"]:
+            print(
+                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
+            )
+
+            peli = KiviPaperiSakset()
+            peli = peli.luo_peli(vastaus)
+            peli.pelaa()
+        else:
+            break
+
+if __name__ == "__main__":
+    main()
